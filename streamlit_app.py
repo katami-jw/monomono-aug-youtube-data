e0860b04be5c4ff83429ed503ac9ca7f20bb4bf5
miyuki katayama <165630128+katami-jw@users.noreply.github.com>
2024-08-27 02:15:19 +0000

    ワードクラウド削除

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,11 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from janome.tokenizer import Tokenizer
-from wordcloud import WordCloud
 import matplotlib.pyplot as plt
+import collections
 
 #テキスト表示
-st.title("Youtubeチャンネル分析")
+st.title("Youtubeチャンネル分析だよ")
 st.write("データをアップロードして気になるチャンネルを分析してみましょう")
 
 #データをアップロード
@@ -22,7 +22,7 @@ if uploaded_file is not None:
     date_start = st.sidebar.date_input('開始日',value=video_data['Post_Date'].min(), min_value=video_data['Post_Date'].min())
     date_end = st.sidebar.date_input('終了日')
     view_min = st.sidebar.number_input('再生数下限',step=10000)
-    like_min = 0
+    like_min = st.sidebar.number_input('高評価数',step=10)
 
     #設定に合わせてデータをフィルター
     data = video_data.query('@date_end >= Post_Date >= @date_start & Views > @view_min & Likes > @like_min')
@@ -37,9 +37,18 @@ if uploaded_file is not None:
     #月別のグラフの表示
     st.subheader('月別の再生数')
     chart_data = data[['Post_Date', 'month', 'Video_Title','Views','Likes','Comments']]
-    st.bar_chart(chart_data, x="month", y=['Views','Likes'])
+    st.bar_chart(chart_data, x="Video_Title", y=['Views','Likes'])
 
-    #テキスト分析（ワードクラウド作成の準備）
+    #matplotlibの表示（散布図）
+    fig1 = plt.figure()
+    ax = fig1.add_subplot(111)
+    ax.scatter(data['Views'], data['Likes'])
+    ax.set_xlabel('view_count')
+    ax.set_ylabel('likes_count')
+
+    st.pyplot(fig1)
+
+    #テキスト分析
     title_text = ''.join(data['Video_Title'])
 
     docs=[]
@@ -49,17 +58,13 @@ if uploaded_file is not None:
         if len(token.base_form) > 2:
             docs.append(token.surface)
     
-    #ワードクラウドの表示
-    c_word = ' '.join(docs)
-    wordcloud = WordCloud(background_color='white',
-                        font_path='Corporate-Logo-Rounded-Bold-ver3.otf',
-                        width=800, height=400).generate(c_word)
+    docs_count=collections.Counter(docs)
+    count_df = pd.DataFrame.from_dict(docs_count, orient='index').reset_index()
+    count_df.columns = ['word', 'count']
 
-    fig = plt.figure(figsize=(10, 5))
-    plt.imshow(wordcloud)
-    plt.axis('off')
+    st.subheader('タイトル頻出ワード')
+    st.dataframe(count_df.sort_values(by='count', ascending=False).head(10),hide_index=True)
 
-    st.pyplot(fig)
 
     #一覧の表示
     st.subheader('動画別のデータ')
